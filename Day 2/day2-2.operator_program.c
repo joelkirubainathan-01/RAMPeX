99f06904b4b57fc8767b58013dc1e5bc77b4ab9b
joelkirubainathan-01 <joelkirubainathansaminathan23csa@vetias.ac.in>
2025-05-21 13:30:44 +0000

    new file:   Day 2/day2-1.operators_proram.c 	new file:   Day 2/day2-2.operator_program.c 	new file:   Day 2/day2-3.simple_intrest.c 	new file:   Day 2/day2-4.oddeven_program.c 	new file:   Day 2/day2-5.leap-year-program.c 	new file:   Day 2/day2-6.greatestamoung-program.c 	new file:   Day 2/day2-7.grading-program.c 	new file:   Day 2/day2-8.finding_greaternum_program.c

---
+++ Day 2/day2-2.operator_program.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int number1=7;
+    int number2=2;
+    int number3=1;
+    int sum=number1*number2/number3;
+    printf("Ans=%d", sum);
+    return 0;
+    
+ }
---
+++ Day 2/day2-6.greatestamoung-program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num1 = 51;
+    int num2 = 45;
+    if (num1 > num2) {
+        printf("It's a greater number among them");
+    } else {
+        printf("It's not a greater number among them");
+    }
+    return 0;
+}
